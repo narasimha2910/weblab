b64cb54f0ec5ed116d99811a11e52bfc016dcbf4
narasimha2910 <2910.narasimha@gmail.com>
2022-07-03 20:29:52 +0530

    Firebase Setup

--- src/components/player.js
+++ src/components/player.js
@@ -3,7 +3,7 @@ import { RiAliensFill } from "react-icons/ri";
 // Have a key in db and localstorage -> Match them to play
 const Player = ({ alienColor }) => {
   return (
-    <div className="flex items-center bg-[#fff] w-5/6 lg:w-full justify-around h-10 mb-2 rounded-md border-[1px] border-[#000] lg:text-sm">
+    <div className="flex items-center bg-[#fff] w-5/6 lg:w-full justify-around h-10 mb-2 lg:mb-4 rounded-md border-[1px] border-[#000] lg:text-sm">
       <div className={`text-3xl`} style={{ color: alienColor }}>
         <RiAliensFill />
       </div>
